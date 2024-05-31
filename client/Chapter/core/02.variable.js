f33abcf573b68f5551f2b5d4918970183088ac0a
kimHyeonJu <kimhyesom1202@gmail.com>
2024-05-31 17:39:55 +0900

    2. variable

---
+++ client/Chapter/core/02.variable.js
@@ -0,0 +1,47 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+// 미니과제 1)
+let admin;
+let name;
+// 또는 let admin, name;
+name = 'John';
+admin = name;
+console.log(admin);
+
+// 미니과제 2)
+let ourPlanet = 'earth';
+let currentUserName = 'kim';
+
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+let products;
+let calculateProductPriceByQuantity; // or calcProductPriceQuantity
+// - 구매 제품 가격의 총 합
+let productsTotalPrice = 20000;
+let totalProductPrice;
+// - 1년 기준 일(day)자 수
+const DAYS = 365;
+const DAY_FER_YEAR = 365;
+// - 구매 결제 여부
+// true / false => boolean data
+let isPayment = false;
+let hasCartList = false;
+let payment = "";
+// - 구매 결제 내역
+let paymentHistory
+// - 브랜드 접두사
+const BRAND_PREFIX = 'Stussy';
+// - 오늘의 운세
+let fortuneToday;  
+// - 상품 정보
+let productInfo;
+
+/* variables ----------------------------------------------------------- */
+
+
+/* constant variables -------------------------------------------------- */
+
